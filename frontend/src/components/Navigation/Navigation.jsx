4a03246d06bc9a408c6621eacd882470a59e5929
CharLee Racine <charleeracine@yahoo.com>
2024-05-23 23:17:27 -0600

    main header navbar coming together some styiling

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import * as sessionActions from '../../store/session'
+import './Navigation.css'
 
 
 
 
 function Navigation(){
     const dispatch = useDispatch;
+
     const handleClick = (e) => {
         e.preventDefault();
         return dispatch(sessionActions.logout())
@@ -16,8 +18,6 @@ function Navigation(){
     const sessionUser = useSelector(state=> state.session.user);
    
 
-   
- 
      
     const sessionLinks = sessionUser ? (
         <>
@@ -25,21 +25,24 @@ function Navigation(){
         </>
     ) : (
         <>
-            <li>
-                <NavLink to='/login'>Log In</NavLink>
-            </li>
-            <li>
-                <NavLink to='/signup'>Sign Up</NavLink>
-            </li>
+                <Link className="userlink" to='/signup'>Sign Up</Link>
+                <Link className="userlink" to='/login'>Log In</Link>
+         
+        
+
+       
         </>
     );
 
 
     return (
         <>
-        <input type='text' value="Shop for used & new music gear" />
-        <button>Sell Your Gear</button>
-       {sessionLinks}
+        <div className="header_main">
+            <img id='logo' src='https://findlogovector.com/wp-content/uploads/2018/11/reverb-logo-vector.png'></img>
+            <input type='text' id='searchbar' value="Shop for used & new music gear" />
+            <button id='sell-button'>Sell Your Gear</button>
+            {sessionLinks}
+       </div>
        </>
     )
 
