5eba3892948db622a2c3a9599181d0eb96d02be7
CharLee Racine <charleeracine@yahoo.com>
2024-05-28 13:32:17 -0600

    deleted other

--- db/migrate/20240525220220_add_model_to_instruments.rb
+++ db/migrate/20240525220220_add_model_to_instruments.rb
@@ -1,4 +1,4 @@
-class AddModelToInstrumentsAgain < ActiveRecord::Migration[7.0]
+class AddModelToInstruments < ActiveRecord::Migration[7.0]
   def change
     add_column :instruments, :model, :string
   end
