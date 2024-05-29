450f2657bfeafe2105e9fa76b50c41f082d9af29
CharLee Racine <charleeracine@yahoo.com>
2024-05-28 22:38:43 -0600

    cart rendering all items in cart properly. bug when logging out.  need to add total, checkout, and rest of cart functionality

--- frontend/src/components/Cart/CartItem.jsx
+++ frontend/src/components/Cart/CartItem.jsx
@@ -9,45 +9,60 @@ function CartItem(){
 
     const cart = useSelector(state => Object.values(state.carts))
 
-    const instrument = useSelector(state => state.instruments[1])
-    
-    return(
-        <>
-            <div id='item-wrapper'>
-                <img className = 'cart-pic' src={instrument.photoUrl} />
-                <div className='cart-item-details'>
-                    <p className='cart-item-name'>{instrument.itemName}</p>
-                    <p className='sold-by'> SOLD BY</p>
-                    <p className='cart-item-seller'>Seller ID: {instrument.sellerId}</p>
-                    <div className='cart-links'>
-                        <div className='move-to-watchlist'>
-                            <IoMdHeart />
-                        <p className='move-words'> Move to Watch List </p>
-                        </div>
-                        <div className='remove'>
-                             <div id='x-wrapper'>
-                                <RiCloseLine />
-                             </div>
-                             <p className='remove-words'>Remove</p>
-                    
-                        </div>
-                    </div>
-
-                   
-                </div>
-                <div className = 'pricing-wrapper'>
-                      <p className='cart-item-price'>{formatter.format(instrument.price)}</p>
-                      <p className='cart-item-shipping'>+ {formatter.format(instrument.shipping)} Shipping</p>
-                      <p className='cart-item-tax'> + applicable tax
-                      <div className='tax-explanation'>
-                        <p id='tax-words'>Tax may be applied during checkout</p>
-                      </div> </p>
-                      
-                </div>
+        const instruments = [];
+        cart.forEach((cartItem)=>{
+            const identifier = cartItem.instrumentId;
+            const instrument = useSelector(state=>state.instruments[identifier])
+            instruments.push(instrument)
+        })
+        
+     
+            return ( 
+                <>
+                <div id='cart-items-only-wrapper'>
+                {instruments.map((instrument)=> { return (
+                    <div id='help'>
+                            <div className='item-wrapper'>
+                              
+                                <img className = 'cart-pic' src={instrument.photoUrl} />
+                                <div className='cart-item-details'>
+                                    <p className='cart-item-name'>{instrument.itemName}</p>
+                                    <p className='sold-by'> SOLD BY</p>
+                                    <p className='cart-item-seller'>Seller ID: {instrument.sellerId}</p>
+                                    <div className='cart-links'>
+                                        <div className='move-to-watchlist'>
+                                            <IoMdHeart />
+                                        <p className='move-words'> Move to Watch List </p>
+                                        </div>
+                                        <div className='remove'>
+                                            <div id='x-wrapper'>
+                                                <RiCloseLine />
+                                            </div>
+                                            <p className='remove-words'>Remove</p>
+                                    
+                                        </div>
+                                    </div>        
+                                </div>
+                            </div>
+                                <div className = 'pricing-wrapper'>
+                                    <p className='cart-item-price'>{formatter.format(instrument.price)}</p>
+                                    <p className='cart-item-shipping'>+ {formatter.format(instrument.shipping)} Shipping</p>
+                                    <p className='cart-item-tax'> + applicable tax
+                                    <div className='tax-explanation'>
+                                        <p id='tax-words'>Tax may be applied during checkout</p>
+                                    </div> </p>
+                                    
+                                </div>
+                         
+               </div>
+               
+            )})
+           
+            
+            }
             </div>
-
-        </>
-    )
+                 </>)
+        
 }
 
 export default CartItem
